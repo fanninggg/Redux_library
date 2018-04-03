58537a3d5c293d4dd50fe7a1ba638c9549728a30
Ben Fanning fanninggg@gmail.com <fanninggg@gmail.com>
2018-04-03 13:35:54 +0100

    created a simple redux app

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function () {
+  return [
+  { title: "Javascript: The Good Parts", pages: 101},
+  { title: "The Northern Lights", pages: 300},
+  { title: "The Subtle Knife", pages: 341},
+  { title: "The Amber Spyglass", pages: 401}
+  ]
+}
+
